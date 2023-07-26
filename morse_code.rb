f18c14a076f0315ef40f4c145fb9bffb9e1e7ba8
akezeth <116703083+akezeth@users.noreply.github.com>
2023-07-26 13:54:58 +0300

    Add the second & third method

--- morse_code.rb
+++ morse_code.rb
@@ -26,8 +26,32 @@ MORSE_CODE = {
   "-.--" => "Y",
   "--.." => "Z"
 }
-def MorseCodeCharConverter(code)
-  puts MORSE_CODE[code]
+
+def decode_char(char)
+  return MORSE_CODE[char]
 end
 
-MorseCodeCharConverter("--")
+def decode_word(word)
+  characters = word.split(" ") # converting the word array of morse characters
+  converted_word = "" # initialize an empty word
+  # converting each code to letters and adding it to the converted_word string variable
+  characters.each { |char| converted_word += decode_char(char) }
+  return converted_word
+end
+
+def decode(code)
+  if code.split(" ").length == 1
+    decode_char(code)
+  elsif code.split("  ").length == 1
+    decode_word(code)
+  else
+    words = code.split("  ") # converting the message to array of morse words
+    converted_message = "" # initialize an empty message
+    # converting each morse code to words and adding them to the converted_message string variable
+    words.each { |word| converted_message += decode_word(word) + " " }
+    return converted_message
+  end
+end
+
+message = decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts message
